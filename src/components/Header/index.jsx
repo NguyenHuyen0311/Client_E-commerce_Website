896b6a1a5d72b877a87e3d4ff1d0efd0c1964074
NguyenHuyen0311 <huyenmeroria@gmail.com>
2025-03-16 23:41:40 +0700

    Header Component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,122 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Search from "../Search";
+import Badge from "@mui/material/Badge";
+import { styled } from "@mui/material/styles";
+import IconButton from "@mui/material/IconButton";
+import { IoCartOutline } from "react-icons/io5";
+import { FaRegHeart } from "react-icons/fa6";
+import Tooltip from "@mui/material/Tooltip";
+import { AiFillPhone } from "react-icons/ai";
+import { FaFacebook , FaInstagram } from "react-icons/fa";
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  "& .MuiBadge-badge": {
+    right: -3,
+    top: 5,
+    border: `1px solid ${theme.palette.background.paper}`,
+    padding: "0 6px 0 4px",
+  },
+}));
+
+function Header() {
+  return (
+    <header>
+      <div className="top-strip py-2 border-t-[1px] border-gray-200 border-b-[1px]">
+        <div className="container">
+          <div className="flex items-center justify-between">
+            <div className="col1 w-[50%]">
+              <p className="text-[12px] font-[400] flex items-center gap-2">
+                <AiFillPhone className="text-[16px]" /> +84 966 556 026 | 
+                <a href="https://fb.com/onimarket" target="_blank" rel="noopener noreferrer" className="flex items-center gap-1 text-blue-600 hover:underline">
+                <FaFacebook className="text-[13px]" /> fb.com/Onimarket
+                </a> | 
+                <a href="https://instagram.com/onimarket" target="_blank" rel="noopener noreferrer" className="flex items-center gap-1 text-pink-500 hover:underline">
+                <FaInstagram className="text-[16px]" /> @Onimarket
+                </a>
+              </p>
+            </div>
+
+            <div className="col2 flex items-center justify-end">
+              <ul className="flex items-center gap-4">
+                <li className="list-none">
+                  <Link
+                    to="help-center"
+                    className="text-[13px] link transition font-[400]"
+                  >
+                    Hỗ trợ
+                  </Link>
+                </li>
+                <li className="list-none">
+                  <Link
+                    to="order-tracking"
+                    className="text-[13px] link transition font-[400]"
+                  >
+                    Theo dõi đơn hàng
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="header">
+        <div className="container flex items-center justify-between">
+          <div className="col1 w-[25%]">
+            <Link to="/" className="logo">
+              <img
+                className="w-40 h-20 object-contain rounded-lg"
+                src="/logo.png"
+              />
+            </Link>
+          </div>
+          <div className="col2 w-[45%]">
+            <Search />
+          </div>
+          <div className="col3 w-[30%] flex items-center pl-7">
+            <ul className="flex items-center justify-end gap-3 w-full">
+              <li className="list-none">
+                <Link
+                  to="/login"
+                  className="text-[15px] link transition font-[400]"
+                >
+                  Đăng nhập
+                </Link>{" "}
+                /{" "}
+                <Link
+                  to="/register"
+                  className="text-[15px] link transition font-[400]"
+                >
+                  Đăng ký
+                </Link>
+              </li>
+
+              <li>
+                <Tooltip title="Yêu thích">
+                  <IconButton aria-label="wishlist">
+                    <StyledBadge badgeContent={4} color="secondary">
+                      <FaRegHeart className="!text-[21px]" />
+                    </StyledBadge>
+                  </IconButton>
+                </Tooltip>
+              </li>
+
+              <li>
+                <Tooltip title="Giỏ hàng">
+                  <IconButton aria-label="cart">
+                    <StyledBadge badgeContent={4} color="secondary">
+                      <IoCartOutline />
+                    </StyledBadge>
+                  </IconButton>
+                </Tooltip>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Search/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './search.css';
+import Button from '@mui/material/Button';
+import { IoSearch } from "react-icons/io5";
+
+function Search() {
+  return (
+    <div className="searchBox w-[100%] h-[50px] bg-[#f2f2f2] rounded-[10px] relative">
+      <input
+        type="text"
+        placeholder="Tìm sản phẩm, thương hiệu và tên shop"
+        className="w-[100%] h-[100%] pl-[20px] pr-[40px] bg-[#f2f2f2] rounded-[10px] focus:outline-none"
+      />
+      <Button className="searchButton !absolute top-0 right-0 h-[100%] w-[40px] !bg-[#ff5252] rounded-[10px] hover:!bg-[#fc6868] transition-colors duration-300 ease-in-out">
+        <IoSearch className="text-white size-5" />
+      </Button>
+    </div>
+  );
+}
+
+export default Search;
