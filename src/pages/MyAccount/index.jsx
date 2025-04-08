f2c4b6f2d30ee55a0078b84107e71f2d8dc7a496
NguyenHuyen0311 <huyenmeroria@gmail.com>
2025-04-08 18:41:10 +0700

    Change Password My Account Page

--- src/pages/MyAccount/index.jsx
+++ src/pages/MyAccount/index.jsx
@@ -4,16 +4,27 @@ import TextField from "@mui/material/TextField";
 import AccountSidebar from "../../components/AccountSideBar";
 import { myContext } from "../../App";
 import { useNavigate } from "react-router-dom";
-import { editData } from "../../utils/api";
+import { editData, postData } from "../../utils/api";
 import { CircularProgress } from "@mui/material";
+import { Collapse } from "react-collapse";
 
 function MyAccount() {
   const [isLoading, setIsLoading] = useState(false);
+  const [isLoading2, setIsLoading2] = useState(false);
   const [userId, setUserId] = useState("");
+  const [isChangePasswordFormShow, setIsChangePasswordFormShow] = useState("");
+
   const [formFields, setFormFields] = useState({
     name: "",
     email: "",
     mobile: "",
+  });
+
+  const [changePassword, setChangePassword] = useState({
+    email: "",
+    oldPassword: "",
+    newPassword: "",
+    confirmPassword: "",
   });
 
   const context = useContext(myContext);
@@ -35,12 +46,23 @@ function MyAccount() {
         email: context?.userData?.email,
         mobile: context?.userData?.mobile,
       });
+
+      setChangePassword({
+        email: context?.userData?.email,
+      });
     }
   }, [context?.userData]);
 
   const onChangeInput = (e) => {
     const { name, value } = e.target;
     setFormFields(() => {
+      return {
+        ...formFields,
+        [name]: value,
+      };
+    });
+
+    setChangePassword(() => {
       return {
         ...formFields,
         [name]: value,
@@ -70,13 +92,53 @@ function MyAccount() {
       return false;
     }
 
-    editData(`/api/user/${userId}`, formFields).then((res) => {
+    editData(`/api/user/${userId}`, formFields, { withCredentials: true }).then(
+      (res) => {
+        if (res?.error !== true) {
+          setIsLoading(false);
+          context.openAlertBox("success", res?.data?.message);
+        } else {
+          context.openAlertBox("error", res?.data?.message);
+          setIsLoading(false);
+        }
+      }
+    );
+  };
+
+  const validateValue2 = Object.values(changePassword).every((el) => el);
+
+  const handleSubmit2 = (e) => {
+    e.preventDefault();
+
+    setIsLoading2(true);
+
+    if (changePassword.newPassword === "") {
+      context.openAlertBox("error", "Vui lòng nhập mật khẩu mới!");
+      return false;
+    }
+
+    if (changePassword.confirmPassword === "") {
+      context.openAlertBox("error", "Vui lòng nhập xác nhận mật khẩu mới!");
+      return false;
+    }
+
+    if (changePassword.newPassword !== changePassword.confirmPassword) {
+      context.openAlertBox(
+        "error",
+        "Mật khẩu mới và xác nhận mật khẩu không trùng khớp!"
+      );
+      return false;
+    }
+
+    postData(`/api/user/reset-password`, changePassword, {
+      withCredentials: true,
+    }).then((res) => {
       if (res?.error !== true) {
-        setIsLoading(false);
-        context.openAlertBox("success", res?.data?.message);
+        setIsLoading2(false);
+        context.openAlertBox("success", res?.message);
       } else {
-        context.openAlertBox("error", res?.data?.message);
-        setIsLoading(false);
+        context.openAlertBox("error", res?.message);
+        setIsLoading2(false);
       }
     });
   };
@@ -90,9 +152,10 @@ function MyAccount() {
 
         <div className="col-2 w-[75%]">
           <div className="card bg-white p-5 shadow-md rounded-md">
-            <h2 className="text-[15px] font-[600] border-b pb-3">
-              Trang cá nhân
-            </h2>
+            <div className="flex items-center justify-between border-b !pb-2">
+              <h2 className="text-[15px] font-[600]">Trang cá nhân</h2>
+              <Button onClick={() => setIsChangePasswordFormShow(!isChangePasswordFormShow)}>Đổi Mật Khẩu</Button>
+            </div>
 
             <form className="mt-5" onSubmit={handleSubmit}>
               <div className="flex items-center gap-5">
@@ -164,6 +227,66 @@ function MyAccount() {
               </div>
             </form>
           </div>
+
+          <Collapse isOpened={isChangePasswordFormShow}>
+            <div className="card bg-white p-5 mt-4 shadow-md rounded-md">
+              <div className="flex items-center justify-between border-b !pb-2">
+                <h2 className="text-[15px] font-[600]">Đổi mật khẩu</h2>
+              </div>
+              <form className="mt-5" onSubmit={handleSubmit2}>
+                <div className="flex items-center gap-5">
+                  <div className="w-[50%]">
+                    <TextField
+                      id="outlined-basic"
+                      label="Mật khẩu mới"
+                      type="text"
+                      variant="outlined"
+                      name="newPassword"
+                      value={changePassword.newPassword}
+                      disabled={isLoading2 === true ? true : false}
+                      size="small"
+                      fullWidth
+                      InputLabelProps={{
+                        style: { fontSize: "14px" },
+                      }}
+                      onChange={onChangeInput}
+                    />
+                  </div>
+                  <div className="w-[50%]">
+                    <TextField
+                      id="outlined-basic"
+                      type="text"
+                      label="Xác nhận mật khẩu"
+                      name="confirmPassword"
+                      value={changePassword.confirmPassword}
+                      disabled={isLoading2 === true ? true : false}
+                      variant="outlined"
+                      size="small"
+                      fullWidth
+                      InputLabelProps={{
+                        style: { fontSize: "14px" },
+                      }}
+                      onChange={onChangeInput}
+                    />
+                  </div>
+                </div>
+
+                <div className="flex items-center mt-5">
+                  <Button
+                    type="submit"
+                    disabled={!validateValue2}
+                    className="btn-org !px-5"
+                  >
+                    {isLoading2 === true ? (
+                      <CircularProgress color="inherit" />
+                    ) : (
+                      "Lưu thay đổi"
+                    )}
+                  </Button>
+                </div>
+              </form>
+            </div>
+          </Collapse>
         </div>
       </div>
     </section>
